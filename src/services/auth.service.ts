51f13ed2c3712e09a7f86ca01f6e0259e50dde0d
BulusHamnu <hamnubulus@gmail.com>
2025-11-26 15:17:23 +0100

    Feat: finish implementing the service that verify if user exist in karma blacklisted pools when signing up

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,11 +1,7 @@
 import db from "../database/db.js";
 import { generateAccessToken } from "../utils/helpers.js";
 import AppError from "../errors/appError.js";
-import {
-  ADJUSTOR_API_BASE,
-  DEMO_CREDIT_ADJUSTOR_API_KEY,
-} from "../config/env.js";
-import axios from "axios";
+import verifyKarmaIdentity from "./verifyKarmaIdentity.js";
 
 export interface User {
   id: number;
@@ -20,19 +16,9 @@ export const createNewUser = async (
   email: string,
   full_name: string
 ): Promise<void> => {
-  // check if user is blacklisted: finish feature later
-  const endpoint = ADJUSTOR_API_BASE + "/verification/karma";
-  // const r = await axios.post(
-  //   endpoint,
-  //   { email },
-  //   {
-  //     headers: {
-  //       "Authorization": `Bearer ${DEMO_CREDIT_ADJUSTOR_API_KEY}`,
-  //       "Content-Type": "application/json",
-  //     },
-  //   }
-  // );
-  // console.log(r.data);
+  // check if user is blacklisted
+  const userIsClean = await verifyKarmaIdentity(email);
+  if (userIsClean) throw new AppError("You can not use this service.", 400);
 
   const userExist = await db("users").where({ email }).first();
   if (userExist) throw new AppError("User already exist.", 409);
---
+++ src/services/verifyKarmaIdentity.ts
@@ -0,0 +1,24 @@
+import {
+  ADJUSTOR_API_BASE,
+  DEMO_CREDIT_ADJUSTOR_API_KEY,
+} from "../config/env.js";
+import axios from "axios";
+
+export default async function verifyKarmaIdentity(
+  identifier: string
+): Promise<boolean> {
+  const endpoint = ADJUSTOR_API_BASE + "/verification/karma/" + identifier;
+
+  const res = await axios.get(endpoint, {
+    headers: {
+      Authorization: `Bearer ${DEMO_CREDIT_ADJUSTOR_API_KEY}`,
+    },
+  });
+
+  // Kyc was not verify i only receive mock data, so this is the solution i decided to use
+  if (res.data.karma_identity === identifier) {
+    return true;
+  }
+
+  return false;
+}

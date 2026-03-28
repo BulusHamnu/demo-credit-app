d8b94fc9d5372a40314ba85643fb039d256103bc
BulusHamnu <hamnubulus@gmail.com>
2026-03-28 19:11:50 +0100

    Fix: updated withAuth middleware to use the new AppError args and error codes.

--- src/middlewares/withAuth.ts
+++ src/middlewares/withAuth.ts
@@ -1,19 +1,26 @@
 import type { Response, Request, NextFunction } from "express";
-import AppError from "../errors/appError.js";
+import AppError, { ErrorCodes } from "../errors/appError.js";
 import db from "../database/db.js";
 import type { User } from "../services/auth.service.js";
 
 const withAuth = async (
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ): Promise<void> => {
   try {
     const token = req.headers["authorization"]?.split(" ")[1];
-    if (!token) throw new AppError("Missing token", 401);
+    if (!token)
+      throw new AppError(ErrorCodes.UNAUTHETICATED, "Missing token", 401, true);
 
     const user: User = await db("users").where({ token }).first();
-    if (!user) throw new AppError("Invalid token, Unauthorized.", 401);
+    if (!user)
+      throw new AppError(
+        ErrorCodes.UNAUTHORIZED,
+        "Invalid token, Unauthorized.",
+        401,
+        true,
+      );
 
     req.user = user;
     next();

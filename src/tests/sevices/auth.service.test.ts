3a1647f607ab186f907e6dfd745d060590af9780
BulusHamnu <hamnubulus@gmail.com>
2026-05-19 21:56:33 +0100

    Fix: Updated tests script for auth and transaction service functions.

--- src/tests/sevices/auth.service.test.ts
+++ src/tests/sevices/auth.service.test.ts
@@ -1,64 +1,123 @@
-// import { vi, expect, test, describe, beforeEach } from "vitest";
-// import { createNewUser, getUser } from "../../../src/services/auth.service.js";
+import { vi, test, expect, describe, beforeEach } from "vitest";
 
-// // mock database
-// let mockWhere = vi.fn().mockReturnThis();
-// let mockFirst = vi.fn().mockResolvedValue(undefined);
-// let mockInsert = vi.fn().mockResolvedValue([1]);
-// let mockUpdate = vi.fn().mockResolvedValue(1);
+const insertMock = vi.fn();
+const firstMock = vi.fn();
+const whereMock = vi.fn(() => ({
+  first: firstMock,
+}));
 
-// vi.mock("../../database/db.js", () => ({
-//   default: vi.fn(() => ({
-//     where: mockWhere,
-//     first: mockFirst,
-//     insert: mockInsert,
-//     update: mockUpdate,
-//   })),
-// }));
+vi.mock("../../database/db.js", () => ({
+  default: vi.fn(() => ({
+    insert: insertMock,
+    where: whereMock,
+  })),
+}));
 
-// // verifyKarmaIdentity return false means user is NOT blacklisted
-// vi.mock("../../services/verifyKarmaIdentity.js", () => ({
-//   default: vi.fn(() => false),
-// }));
+vi.mock("../../utils/helpers.js", () => ({
+  generateAccessToken: vi.fn(),
+}));
 
-// // mock function for generating access token
-// vi.mock("../../utils/helpers.js", () => ({
-//   generateAccessToken: vi.fn(() => "a43e8462-3dbb-4ece-9303-e2076237b7f3"),
-// }));
+vi.mock("../../services/verifyKarmaIdentity.js", () => ({
+  default: vi.fn(),
+}));
 
-// import { generateAccessToken } from "../../../src/utils/helpers.js";
-// import verifyKarmaIdentity from "../../../src/services/verifyKarmaIdentity.js";
-// import db from "../../../src/database/db.js";
+import * as authService from "../../services/auth.service.js";
+import { generateAccessToken } from "../../utils/helpers.js";
+import AppError, { ErrorCodes } from "../../errors/appError.js";
 
-// // test
-// describe("Auth Service Tests", () => {
-//   test("Create new user", async () => {
-//     await createNewUser("hamnubulus@gmail.com", "Bulus Hamnu");
-//     // verify karma was checked
-//     expect(verifyKarmaIdentity).toHaveBeenCalledWith("hamnubulus@gmail.com");
+describe("Auth service tests", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
 
-//     // db was queried to check for existing user
-//     expect(db).toHaveBeenCalledWith("users");
-//     expect(mockFirst).toHaveBeenCalled();
+  describe("Create new user func", () => {
+    test("User created successfully", async () => {
+      (generateAccessToken as any).mockReturnValue(
+        "3153e291-926e-4319-8086-0221280c7d78",
+      );
 
-//     expect(mockWhere).toHaveBeenCalledWith({ email: "hamnubulus@gmail.com" });
-//     // token was generated
-//     expect(generateAccessToken).toHaveBeenCalled();
+      insertMock.mockResolvedValue([5]);
 
-//     // user was inserted
-//     expect(mockInsert).toHaveBeenCalledWith({
-//       email: "hamnubulus@gmail.com",
-//       full_name: "Bulus Hamnu",
-//       token: "a43e8462-3dbb-4ece-9303-e2076237b7f3",
-//     });
-//   });
+      await authService.createNewUser("hamnu@gmail.com", "Bulus Hamnu");
 
-//   test("Get user", async () => {
-//     mockFirst.mockResolvedValueOnce({ email: "hamnubulus@gmail.com" });
-//     await getUser("hamnubulus@gmail.com");
+      expect(generateAccessToken).toHaveBeenCalled();
 
-//     // db was queried for the user
-//     expect(db).toHaveBeenCalledWith("users");
-//     expect(mockFirst).toHaveBeenCalled();
-//   });
-// });
+      expect(insertMock).toHaveBeenCalledWith({
+        email: "hamnu@gmail.com",
+        full_name: "Bulus Hamnu",
+        token: "3153e291-926e-4319-8086-0221280c7d78",
+      });
+    });
+
+    test("User already exists", async () => {
+      const dupError: any = {
+        errno: 1062,
+      };
+
+      insertMock.mockRejectedValue(dupError);
+
+      await expect(
+        authService.createNewUser("hamnu@gmail.com", "Bulus Hamnu"),
+      ).rejects.toBeInstanceOf(AppError);
+
+      await expect(
+        authService.createNewUser("hamnu@gmail.com", "Bulus Hamnu"),
+      ).rejects.toMatchObject({
+        message: "User already exists.",
+        code: ErrorCodes.USER_ALREADY_EXISTS,
+      });
+    });
+
+    test("Should throw unknown database errors", async () => {
+      const databaseError = new Error("Database crashed");
+
+      insertMock.mockRejectedValue(databaseError);
+
+      await expect(
+        authService.createNewUser("hamnu@gmail.com", "Bulus Hamnu"),
+      ).rejects.toThrow("Database crashed");
+    });
+  });
+
+  describe("Retrieve user token func", () => {
+    test("User token retrieved successfully", async () => {
+      firstMock.mockResolvedValue({
+        id: 5,
+        email: "hamnu@gmail.com",
+        token: "3153e291-926e-4319-8086-0221280c7d78",
+      });
+
+      const result = await authService.retrieveUserToken("hamnu@gmail.com");
+
+      expect(whereMock).toHaveBeenCalledWith({
+        email: "hamnu@gmail.com",
+      });
+      expect(result).toBe("3153e291-926e-4319-8086-0221280c7d78");
+    });
+
+    test("User not found", async () => {
+      firstMock.mockResolvedValue(undefined);
+
+      await expect(
+        authService.retrieveUserToken("hamnu@gmail.com"),
+      ).rejects.toBeInstanceOf(AppError);
+
+      await expect(
+        authService.retrieveUserToken("hamnu@gmail.com"),
+      ).rejects.toMatchObject({
+        message: "User not found.",
+        code: ErrorCodes.USER_NOT_FOUND,
+      });
+    });
+
+    test("Should handle database errors", async () => {
+      const fakeError = new Error("Database crashed");
+
+      firstMock.mockRejectedValue(fakeError);
+
+      await expect(
+        authService.retrieveUserToken("hamnu@gmail.com"),
+      ).rejects.toThrow("Database crashed");
+    });
+  });
+});
--- src/tests/sevices/transactions.service.test.ts
+++ src/tests/sevices/transactions.service.test.ts
@@ -1,190 +1,119 @@
-// import { vi, expect, test, describe } from "vitest";
+import { vi, test, expect, describe, beforeEach } from "vitest";
 
-// // mocks
-// let mockWhere = vi.fn().mockReturnThis();
-// let mockFirst = vi.fn();
-// let mockIncrement = vi.fn().mockResolvedValue(1);
-// let mockDecrement = vi.fn().mockResolvedValue(1);
-// let mockInsert = vi.fn().mockResolvedValue([1]);
+const insertMock = vi.fn();
+const whereMock = vi.fn();
 
-// // trx mock
-// let mockTrx = vi.fn((table) => ({
-//   where: mockWhere,
-//   first: mockFirst,
-//   increment: mockIncrement,
-//   decrement: mockDecrement,
-//   insert: mockInsert,
-// }));
+vi.mock("../../database/db.js", () => ({
+  default: vi.fn(() => ({
+    insert: insertMock,
+    where: whereMock,
+  })),
+}));
 
-// // db mock
-// vi.mock("../../database/db.js", () => {
-//   const dbMock: any = vi.fn((table) => ({
-//     where: mockWhere,
-//     first: mockFirst,
-//     increment: mockIncrement,
-//     decrement: mockDecrement,
-//     insert: mockInsert,
-//   }));
+vi.mock("../../utils/helpers.js", () => ({
+  generateTransactionReference: vi.fn(),
+}));
 
-//   dbMock.transaction = vi.fn(async (cb) => {
-//     await cb(mockTrx);
-//   });
+import * as transactionService from "../../services/transactions.service.js";
+import { generateTransactionReference } from "../../utils/helpers.js";
 
-//   return { default: dbMock };
-// });
+describe("Transaction service tests", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
 
-// // helpers mock
-// vi.mock("../../utils/helpers.js", () => ({
-//   generateTransactionReference: vi.fn(() => "TNX_12345"),
-// }));
+  describe("Record transaction func", () => {
+    test("Transaction recorded successfully", async () => {
+      const fakeDb: any = vi.fn(() => ({
+        insert: insertMock,
+      }));
 
-// import {
-//   transferFunds,
-//   getUserTransactions,
-//   tranferToWallet,
-// } from "../../../src/services/transactions.service.js";
-// import db from "../../../src/database/db.js";
-// import AppError from "../../../src/errors/appError.js";
+      (generateTransactionReference as any).mockReturnValue(
+        "TNX_123456FHR454RDFDF45TTFFE3E3",
+      );
 
-// describe("Transaction Service Tests", () => {
-//   // transferFunds tests
-//   test("Transfer funds: deposit", async () => {
-//     mockWhere.mockClear();
-//     mockFirst.mockClear();
-//     mockIncrement.mockClear();
-//     mockInsert.mockClear();
+      insertMock.mockResolvedValue([1]);
 
-//     // mock wallet
-//     mockFirst.mockResolvedValueOnce({
-//       id: 10,
-//       balance: 1000,
-//       address: "wallet22334455",
-//       user_id: 1,
-//     });
+      await transactionService.recordTransaction({
+        db: fakeDb,
+        userId: 5,
+        amount: 5000,
+        receiverWalletId: 2,
+        senderWalletId: 1,
+        type: "deposit",
+        notes: "deposit note",
+      });
 
-//     const result = await transferFunds(200, "wallet22334455", "deposit", 1);
+      expect(generateTransactionReference).toHaveBeenCalled();
 
-//     expect(mockWhere).toHaveBeenCalledWith({
-//       address: "wallet22334455",
-//       user_id: 1,
-//     });
-//     expect(mockIncrement).toHaveBeenCalledWith("balance", 200);
-//     expect(mockInsert).toHaveBeenCalled();
+      expect(insertMock).toHaveBeenCalledWith({
+        amount: 5000,
+        receiver_wallet_id: 2,
+        sender_wallet_id: 1,
+        type: "deposit",
+        notes: "deposit note",
+        reference: "TNX_123456FHR454RDFDF45TTFFE3E3",
+        initiated_by: 5,
+      });
+    });
 
-//     expect(result).toBe(true);
-//   });
+    test("Should handle transaction record errors", async () => {
+      const fakeDb: any = vi.fn(() => ({
+        insert: insertMock,
+      }));
 
-//   test("Transfer funds: withdrawal insufficient balance", async () => {
-//     mockFirst.mockClear();
-//     mockFirst.mockResolvedValueOnce({
-//       id: 10,
-//       balance: 50,
-//       address: "wallet22334455",
-//       user_id: 1,
-//     });
+      const databaseError = new Error("Database crashed");
 
-//     await expect(
-//       transferFunds(100, "wallet22334455", "withdrawal", 1),
-//     ).rejects.toThrow(AppError);
-//   });
+      insertMock.mockRejectedValue(databaseError);
 
-//   test("Transfer funds: wallet not found", async () => {
-//     mockFirst.mockClear();
-//     mockFirst.mockResolvedValueOnce(undefined);
+      await expect(
+        transactionService.recordTransaction({
+          db: fakeDb,
+          userId: 5,
+          amount: 5000,
+          receiverWalletId: 2,
+          senderWalletId: 1,
+          type: "deposit",
+          notes: "deposit note",
+        }),
+      ).rejects.toThrow("Database crashed");
+    });
+  });
 
-//     await expect(
-//       transferFunds(200, "wallet22334455", "deposit", 1),
-//     ).rejects.toThrow(AppError);
-//   });
+  describe("Get user transactions func", () => {
+    test("Transactions retrieved successfully", async () => {
+      const transactions = [
+        {
+          id: 1,
+          amount: 5000,
+          initiated_by: 5,
+          type: "deposit",
+        },
+        {
+          id: 2,
+          amount: 2000,
+          initiated_by: 5,
+          type: "withdrawal",
+        },
+      ];
 
-//   // getUserTransactions tests
-//   test("Get user transactions", async () => {
-//     mockWhere.mockClear();
+      whereMock.mockResolvedValue(transactions);
 
-//     (db as any).mockReturnValueOnce({
-//       where: mockWhere.mockResolvedValueOnce([
-//         { id: 1, amount: 200, initiated_by: 1 },
-//       ]),
-//     });
+      const result = await transactionService.getUserTransactions(5);
+      expect(whereMock).toHaveBeenCalledWith({
+        initiated_by: 5,
+      });
 
-//     const result = await getUserTransactions(1);
+      expect(result).toEqual(transactions);
+    });
 
-//     expect(db).toHaveBeenCalledWith("transactions");
-//     expect(mockWhere).toHaveBeenCalledWith({ initiated_by: 1 });
+    test("Should handle database errors", async () => {
+      const databaseError = new Error("Database crashed");
 
-//     expect(result).toEqual([{ id: 1, amount: 200, initiated_by: 1 }]);
-//   });
-
-//   // tranferToWallet tests
-//   test("Transfer to wallet: successful", async () => {
-//     mockFirst.mockClear();
-//     mockWhere.mockClear();
-//     mockIncrement.mockClear();
-//     mockDecrement.mockClear();
-//     mockInsert.mockClear();
-
-//     // sender wallet
-//     mockFirst.mockResolvedValueOnce({
-//       id: 1,
-//       balance: 500,
-//       address: "SENDER123",
-//     });
-
-//     // receiver wallet
-//     mockFirst.mockResolvedValueOnce({
-//       id: 2,
-//       balance: 100,
-//       address: "RECEIVER999",
-//     });
-
-//     const result = await tranferToWallet(200, "RECEIVER999", "", 5);
-
-//     expect(db).toHaveBeenCalledWith("wallets");
-//     expect(mockWhere).toHaveBeenCalledWith({ address: "RECEIVER999" });
-//     expect(mockDecrement).toHaveBeenCalledWith("balance", 200);
-//     expect(mockIncrement).toHaveBeenCalledWith("balance", 200);
-//     expect(mockInsert).toHaveBeenCalled();
-
-//     expect(result).toBe(true);
-//   });
-
-//   test("Transfer to wallet: sender wallet not found", async () => {
-//     mockFirst.mockClear();
-//     mockFirst.mockResolvedValueOnce(undefined);
-
-//     await expect(tranferToWallet(100, "fvsuvcusc", "", 10)).rejects.toThrow(
-//       AppError,
-//     );
-//   });
-
-//   test("Transfer to wallet: insufficient balance", async () => {
-//     mockFirst.mockClear();
-//     mockFirst.mockResolvedValueOnce({ id: 1, balance: 50 });
-
-//     await expect(tranferToWallet(200, "any-wallet", "", 5)).rejects.toThrow(
-//       AppError,
-//     );
-//   });
-
-//   test("Transfer to wallet: receiver wallet not found", async () => {
-//     mockFirst.mockClear();
-
-//     mockFirst
-//       .mockResolvedValueOnce({ id: 1, balance: 500 })
-//       .mockResolvedValueOnce(undefined);
-
-//     await expect(tranferToWallet(100, "UNKNOWN", "", 5)).rejects.toThrow(
-//       AppError,
-//     );
-//   });
-
-//   test("Transfer to wallet: same wallet error", async () => {
-//     mockFirst.mockClear();
-
-//     mockFirst
-//       .mockResolvedValueOnce({ id: 1, balance: 500, address: "W1" })
-//       .mockResolvedValueOnce({ id: 1, address: "W1" });
-
-//     await expect(tranferToWallet(50, "W1", "", 5)).rejects.toThrow(AppError);
-//   });
-// });
+      whereMock.mockRejectedValue(databaseError);
+      await expect(transactionService.getUserTransactions(5)).rejects.toThrow(
+        "Database crashed",
+      );
+    });
+  });
+});

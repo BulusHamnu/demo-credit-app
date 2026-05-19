3dee54d961fc1d683e8032f74922bc1d6b54891b
BulusHamnu <hamnubulus@gmail.com>
2026-05-19 17:13:13 +0100

    Fix: Updated transaction controller tests for transaction controller functions.

--- src/tests/controllers/transaction.controllers.test.ts
+++ src/tests/controllers/transaction.controllers.test.ts
@@ -1,238 +1,85 @@
-// import { vi, test, expect, describe } from "vitest";
+import { vi, test, expect, describe, beforeEach } from "vitest";
 
-// // mock all transaction services
-// vi.mock("../../services/transactions.service.js", () => ({
-//   transferFunds: vi.fn(),
-//   getUserTransactions: vi.fn(),
-//   tranferToWallet: vi.fn(),
-// }));
+vi.mock("../../services/transactions.service.js", () => ({
+  getUserTransactions: vi.fn(),
+}));
 
-// import {
-//   depositController,
-//   getTransactionsController,
-//   transferController,
-//   withdrawController,
-// } from "../../../src/controllers/transaction.controllers.js";
+import * as transactionController from "../../controllers/transaction.controllers.js";
+import * as transactionService from "../../services/transactions.service.js";
 
-// import {
-//   transferFunds,
-//   getUserTransactions,
-//   tranferToWallet,
-// } from "../../../src/services/transactions.service.js";
+describe("Transaction controller tests", () => {
+  let req: any;
+  let res: any;
+  let next: any;
 
-// import AppError from "../../../src/errors/appError.js";
+  beforeEach(() => {
+    vi.clearAllMocks();
 
-// const mockedTransferFunds = transferFunds as unknown as ReturnType<
-//   typeof vi.fn
-// >;
-// const mockedGetUserTransactions = getUserTransactions as unknown as ReturnType<
-//   typeof vi.fn
-// >;
-// const mockedTransferToWallet = tranferToWallet as unknown as ReturnType<
-//   typeof vi.fn
-// >;
+    req = {};
 
-// describe("Transaction Controller Tests", () => {
-//   // Deposit Tests
-//   test("Deposit: successful", async () => {
-//     mockedTransferFunds.mockClear();
+    res = {
+      status: vi.fn().mockReturnThis(),
+      json: vi.fn(),
+    };
 
-//     const req: any = {
-//       user: { id: 10 },
-//       body: {
-//         amount: 500,
-//         address: "8350a25d32394bbcb95464f73537c377",
-//         notes: "Test deposit",
-//       },
-//     };
+    next = vi.fn();
+  });
 
-//     const json = vi.fn();
-//     const status = vi.fn(() => ({ json }));
-//     const res: any = { status };
-//     const next = vi.fn();
+  describe("Get transactions func", () => {
+    test("Transactions retrieved successfully", async () => {
+      const transactions = [
+        {
+          id: 1,
+          amount: 5000,
+          initiated_by: 5,
+          type: "deposit",
+        },
+        {
+          id: 2,
+          amount: 2000,
+          initiated_by: 5,
+          type: "withdrawal",
+        },
+      ];
 
-//     await depositController(req, res, next);
+      req = {
+        user: {
+          id: 5,
+        },
+      };
 
-//     expect(mockedTransferFunds).toHaveBeenCalledWith(
-//       500,
-//       "8350a25d32394bbcb95464f73537c377",
-//       "deposit",
-//       10,
-//     );
+      (transactionService.getUserTransactions as any).mockResolvedValue(
+        transactions,
+      );
 
-//     expect(status).toHaveBeenCalledWith(200);
-//     expect(json).toHaveBeenCalledWith({
-//       status: true,
-//       message: "Deposit of 500 was successful.",
-//     });
+      await transactionController.getTransactions(req, res, next);
 
-//     expect(next).not.toHaveBeenCalled();
-//   });
+      expect(transactionService.getUserTransactions).toHaveBeenCalledWith(5);
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith({
+        status: true,
+        message: "Transactions retrieved successfully.",
+        data: transactions,
+      });
+      expect(next).not.toHaveBeenCalled();
+    });
 
-//   test("Deposit: missing fields", async () => {
-//     mockedTransferFunds.mockClear();
+    test("Should handle transaction retrieval errors", async () => {
+      req = {
+        user: {
+          id: 5,
+        },
+      };
 
-//     const req: any = {
-//       user: { id: 10 },
-//       body: {
-//         amount: null,
-//         address: "",
-//       },
-//     };
+      const databaseError = new Error("Database crashed");
+      (transactionService.getUserTransactions as any).mockRejectedValue(
+        databaseError,
+      );
 
-//     const next = vi.fn();
-//     const res: any = {};
+      await transactionController.getTransactions(req, res, next);
 
-//     await depositController(req, res, next);
-
-//     expect(next).toHaveBeenCalled();
-//     const error = (next as any).mock.calls[0][0];
-//     expect(error).toBeInstanceOf(AppError);
-//   });
-
-//   // Get User Transactions Tests
-//   test("Get Transactions: successful", async () => {
-//     mockedGetUserTransactions.mockClear();
-
-//     mockedGetUserTransactions.mockResolvedValueOnce([
-//       { id: 1, amount: 200 },
-//       { id: 2, amount: 500 },
-//     ]);
-
-//     const req: any = { user: { id: 10 } };
-//     const json = vi.fn();
-//     const status = vi.fn(() => ({ json }));
-//     const res: any = { status };
-//     const next = vi.fn();
-
-//     await getTransactionsController(req, res, next);
-
-//     expect(mockedGetUserTransactions).toHaveBeenCalledWith(10);
-
-//     expect(status).toHaveBeenCalledWith(200);
-//     expect(json).toHaveBeenCalledWith({
-//       status: true,
-//       message: "Transactions retrived successfully.",
-//       data: [
-//         { id: 1, amount: 200 },
-//         { id: 2, amount: 500 },
-//       ],
-//     });
-
-//     expect(next).not.toHaveBeenCalled();
-//   });
-
-//   // Transfer Tests
-//   test("Transfer: successful", async () => {
-//     mockedTransferToWallet.mockClear();
-
-//     const req: any = {
-//       user: { id: 7 },
-//       body: {
-//         amount: 300,
-//         address: "8350a25d32394bbcb95464f73537c377",
-//         notes: "test transfer",
-//       },
-//     };
-
-//     const json = vi.fn();
-//     const status = vi.fn(() => ({ json }));
-//     const res: any = { status };
-//     const next = vi.fn();
-
-//     await transferController(req, res, next);
-
-//     expect(mockedTransferToWallet).toHaveBeenCalledWith(
-//       300,
-//       "8350a25d32394bbcb95464f73537c377",
-//       "test transfer",
-//       7,
-//     );
-
-//     expect(status).toHaveBeenCalledWith(200);
-//     expect(json).toHaveBeenCalledWith({
-//       status: true,
-//       message: "Transfer was successful.",
-//     });
-
-//     expect(next).not.toHaveBeenCalled();
-//   });
-
-//   test("Transfer: missing fields", async () => {
-//     mockedTransferToWallet.mockClear();
-
-//     const req: any = {
-//       user: { id: 7 },
-//       body: {
-//         amount: null,
-//         address: "",
-//         notes: "",
-//       },
-//     };
-
-//     const res: any = {};
-//     const next = vi.fn();
-
-//     await transferController(req, res, next);
-
-//     expect(next).toHaveBeenCalled();
-//     const error = (next as any).mock.calls[0][0];
-//     expect(error).toBeInstanceOf(AppError);
-//   });
-
-//   // Withdrawal Tests
-//   test("Withdrawal: successful", async () => {
-//     mockedTransferFunds.mockClear();
-
-//     const req: any = {
-//       user: { id: 20 },
-//       body: {
-//         amount: 450,
-//         address: "8350a25d32394bbcb95464f73537c377",
-//       },
-//     };
-
-//     const json = vi.fn();
-//     const status = vi.fn(() => ({ json }));
-//     const res: any = { status };
-//     const next = vi.fn();
-
-//     await withdrawController(req, res, next);
-
-//     expect(mockedTransferFunds).toHaveBeenCalledWith(
-//       450,
-//       "8350a25d32394bbcb95464f73537c377",
-//       "withdrawal",
-//       20,
-//     );
-
-//     expect(status).toHaveBeenCalledWith(200);
-//     expect(json).toHaveBeenCalledWith({
-//       status: true,
-//       message: "Withdrawal was successfully.",
-//     });
-
-//     expect(next).not.toHaveBeenCalled();
-//   });
-
-//   test("Withdrawal: missing fields", async () => {
-//     mockedTransferFunds.mockClear();
-
-//     const req: any = {
-//       user: { id: 20 },
-//       body: {
-//         amount: null,
-//         address: "",
-//       },
-//     };
-
-//     const res: any = {};
-//     const next = vi.fn();
-
-//     await withdrawController(req, res, next);
-
-//     expect(next).toHaveBeenCalled();
-//     const error = (next as any).mock.calls[0][0];
-//     expect(error).toBeInstanceOf(AppError);
-//   });
-// });
+      expect(transactionService.getUserTransactions).toHaveBeenCalledWith(5);
+      expect(next).toHaveBeenCalledWith(databaseError);
+    });
+  });
+});
